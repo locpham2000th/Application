2784d9b0c62d15524b387cd8898b26e14ee91d32
Lộc Phạm <phamxuanloc2000@gmail.com>
2020-10-17 12:03:46 +0700

    first commit

---
+++ src/main/java/com/example/it3180/It3180Application.java
@@ -0,0 +1,13 @@
+package com.example.it3180;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class It3180Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(It3180Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/it3180/It3180ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.it3180;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class It3180ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
